6a383029707aa1f17de01ed66406b599e00225ea
Rajatrar <rajat592002@gmail.com>
2024-07-17 04:38:24 +0530

    Day2_Solution_added

---
+++ 02.js
@@ -0,0 +1,38 @@
+// Activity 1
+let num1 = 12;
+let num2 = 4;
+
+console.log(`Addition: ${num1 + num2}`);       
+console.log(`Subtraction: ${num1 - num2}`);    
+console.log(`Multiplication: ${num1 * num2}`); 
+console.log(`Division: ${num1 / num2}`);      
+console.log(`Remainder: ${num1 % num2}`);   
+
+// Activity 2
+console.log("Present number:- ",num1)
+console.log("Updated added Number:- ", num1 += num2)
+console.log("Updated Subtracted Number:- ", num1 -= num2)
+
+// Activity 3
+
+console.log(`num1 > num2: ${num1 > num2}`);   
+console.log(`num1 < num2: ${num1 < num2}`);  
+
+console.log(`num1 >= num2: ${num1 >= num2}`);  
+console.log(`num1 <= num2: ${num1 <= num2}`); 
+
+console.log(`num1 == num2: ${num1 == num2}`);  
+console.log(`num1 === num2: ${num1 === num2}`);
+
+// Activity 4
+
+console.log(`num1 < 10 && num2 > 10: ${num1 < 10 && num2 > 10}`); 
+console.log(`num1 < 10 || num2 > 15: ${num1 < 10 || num2 > 15}`); 
+console.log(`!(num1 > num2): ${!(num1 > num2)}`);
+
+// Activity 5
+let num = 5;
+
+let result = num >= 0 ? "Positive" : "Negative";
+
+console.log(`The number ${num} is ${result}.`);
