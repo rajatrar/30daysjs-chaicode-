ce85852a10b4a9831bf59aaa864d09cf47697e4f
Rajatrar <rajat592002@gmail.com>
2024-07-17 04:25:55 +0530

    day1_Solution

---
+++ 01.js
@@ -0,0 +1,30 @@
+// Task 1
+var num = 1;
+console.log(num);
+// Task 2
+let name = "rajat"
+console.log(name)
+
+// Task 3
+const boolean = true
+console.log(boolean);
+
+// Task 4
+const object = {
+    name: "rajat",
+}
+
+const array = [1,2,3];
+console.log(typeof(num));
+console.log(typeof(name));
+console.log(typeof(boolean));
+console.log(typeof(object));
+console.log(typeof(array));
+
+// Task 5 
+
+let reassignableLet = "Rajat";
+console.log('initial :', reassignableLet);
+
+reassignableLet = "Harshu";
+console.log('reassigned :', reassignableLet);
